bb8f16bb9465f939cf6c52684f068e778692d235
Kursant Anarkulov <anarkursant08@gmail.com>
2026-03-22 22:16:44 +0600

    Opened ui-component and added a universal import file to shorten the path.

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { App } from './App.jsx'
+import { ThemeProvider } from '@mui/material'
+import { colors } from './utils/colors.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={colors}>  
+     <App />
+    </ThemeProvider>  
   </StrictMode>
 )
