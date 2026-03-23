dbc90c327b2ecc50364fefba82aacea304d5f8cb
Ymanbek Baltabaev Eralievich <ymanbekbaltabaev17@gmail.com>
2026-03-24 01:54:13 +0600

    стиль коштук

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -3,8 +3,28 @@ import { forwardRef } from 'react'
 
 export const Button = forwardRef(
   ({ children, variant, type = 'button', disabled, icon, ...rest }, ref) => (
-    <MuiButton variant={variant} ref={ref} type={type} disabled={disabled} {...rest}>
-      {icon} {children}
-    </MuiButton>
+    <StyledButton variant={variant} ref={ref} type={type} disabled={disabled} {...rest}>
+      {icon && '+'} {children}
+    </StyledButton>
   )
 )
+
+const StyledButton = styled(MuiButton)(() => ({
+  backgroundColor: '#FFD519',
+  color: '#000000',
+  padding: '8px 20px',
+  borderRadius: '10px',
+
+  '&:hover': {
+    backgroundColor: '#FFD519',
+  },
+
+  '&:active': {
+    backgroundColor: '#FFD519',
+  },
+
+  '&.Mui-disabled': {
+    backgroundColor: '#cfcfcf',
+    color: '#888',
+  },
+}))
