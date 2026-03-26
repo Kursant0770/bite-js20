c6017de1a5cf4dea1f3d1a83b01acfd84297d49b
mahamatalievelmar-arch <mahamatalievelmar@gmail.com>
2026-03-26 19:13:32 +0600

    update Select, Button, Input components

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,14 @@
-export const App = () => <h1>Bite</h1>
+import { useState } from 'react'
+import { Select } from './components/ui/select/Select'
+import { options } from './options'
+
+export const App = () => {
+  const [selected, setSelected] = useState(null)
+
+  return (
+    <div>
+      <h1>Bite</h1>
+      <Select options={options} value={selected} onChange={setSelected} placeholder="Выберите..." />
+    </div>
+  )
+}
