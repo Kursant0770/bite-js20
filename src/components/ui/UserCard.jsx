52cde9e131dd30ab03113ed34ce1000db9c5780d
Ruslan-007 <erkebaevruslan385@gmail.com>
2026-03-29 22:57:12 +0600

    fixed-cards

--- src/components/ui/UserCard.jsx
+++ src/components/ui/UserCard.jsx
@@ -1,16 +1,10 @@
-import { useState } from "react";
-import { styled } from "@mui/material/styles"; 
-import { Button } from "../ui/Button";
+import { useState } from 'react'
+import { styled } from '@mui/material/styles'
+import { Button } from '../ui/Button'
 
-export const UserCard = ({
-  title,
-  price,
-  oldPrice,
-  weight,
-  image,
-}) => {
-  const [count, setCount] = useState(0);
-  const hasDiscount = !!(oldPrice && oldPrice > price);
+export const UserCard = ({ title, price, oldPrice, weight, image }) => {
+  const [count, setCount] = useState(0)
+  const hasDiscount = !!(oldPrice && oldPrice > price)
 
   return (
     <Card>
@@ -20,111 +14,128 @@ export const UserCard = ({
       </ImageWrapper>
 
       <PriceRow>
+        <CurrentPrice isDiscount={hasDiscount}>{price} сом</CurrentPrice>
 
-        <CurrentPrice isDiscount={hasDiscount}>
-
-          {price} сом
-        </CurrentPrice>
-
-        {hasDiscount && (
-          <OldPrice>{oldPrice} сом</OldPrice>
-        )}
+        {hasDiscount && <OldPrice>{oldPrice} сом</OldPrice>}
       </PriceRow>
 
       <Title>{title}</Title>
       <Weight>{weight} г</Weight>
 
       {count === 0 ? (
-        <Button 
+        <Button
           onClick={() => setCount(1)}
           variant="outlined"
-          style={{ width: "100%", marginTop: "10px",height: "38px" }}
+          style={{ width: '100%', marginTop: '10px', height: '38px' }}
         >
           + Добавить
         </Button>
       ) : (
         <Counter>
-          <Button onClick={() => setCount(count - 1)}>-</Button>
+          <Button
+            onClick={() => setCount(count - 1)}
+            sx={{
+              minWidth: 'auto',
+              border: 'none',
+              background: 'transparent',
+              boxShadow: 'none',
+              fontSize: '18px',
+            }}
+          >
+            -
+          </Button>
           <span>{count}</span>
-          <Button onClick={() => setCount(count + 1)}>+</Button>
+          <Button
+            onClick={() => setCount(count + 1)}
+            sx={{
+              minWidth: 'auto',
+              border: 'none',
+              background: 'transparent',
+              boxShadow: 'none',
+              fontSize: '18px',
+            }}
+          >
+            +
+          </Button>
         </Counter>
       )}
     </Card>
-  );
-};
+  )
+}
 
-const Card = styled("div")({
-  width: "210px",
-  backgroundColor: "#F5F4F2",
-  borderRadius: "10px",
-  padding: "10px",
-});
+const Card = styled('div')({
+  width: '210px',
+  backgroundColor: '#F5F4F2',
+  borderRadius: '10px',
+  padding: '10px',
+})
 
-const ImageWrapper = styled("div")({
-  position: "relative",
-  "& img": {
-    width: "190px",
-    height: "168px",
-    borderRadius: "10px",
-    objectFit: "cover",
+const ImageWrapper = styled('div')({
+  position: 'relative',
+  '& img': {
+    width: '190px',
+    height: '168px',
+    borderRadius: '10px',
+    objectFit: 'cover',
   },
-});
+})
 
-const Discount = styled("div")({
-  position: "absolute",
-  bottom: "8px",
-  right: "8px",
-  backgroundColor: "#4caf50",
-  color: "white",
-  padding: "4px",
-  borderRadius: "6px",
-  fontSize: "10px",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  height: "15px"
-});
+const Discount = styled('div')({
+  position: 'absolute',
+  bottom: '8px',
+  right: '8px',
+  backgroundColor: '#4caf50',
+  color: 'white',
+  padding: '4px',
+  borderRadius: '6px',
+  fontSize: '10px',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  height: '15px',
+})
 
-const PriceRow = styled("div")({
-  display: "flex",
-  gap: "8px",
-  alignItems: "center",
-  marginTop: "8px",
-});
+const PriceRow = styled('div')({
+  display: 'flex',
+  gap: '8px',
+  alignItems: 'center',
+  marginTop: '8px',
+})
 
-const CurrentPrice = styled("span", {
-  shouldForwardProp: (prop) => prop !== "isDiscount",
+const CurrentPrice = styled('span', {
+  shouldForwardProp: (prop) => prop !== 'isDiscount',
 })(({ isDiscount }) => ({
-   fontSize: "18px",
-  fontWeight: "400",             
-  fontFamily: "Helvetica, sans-serif",  // 👈
-  color: isDiscount ? "#ff6b00" : "#000",
-}));
+  fontSize: '18px',
+  fontWeight: '400',
+  fontFamily: 'Helvetica, sans-serif',
+  color: isDiscount ? '#ff6b00' : '#000',
+}))
 
-const OldPrice = styled("span")({
-  textDecoration: "line-through",
-  color: "#999",
-});
+const OldPrice = styled('span')({
+  textDecoration: 'line-through',
+  color: '#999',
+})
 
-const Title = styled("div")({
-  marginTop: "6px",
-  fontSize: "18px",
-  fontWeight: "500",
-  fontFamily: "Helvetica, sans-serif",
-});
+const Title = styled('div')({
+  marginTop: '6px',
+  fontSize: '18px',
+  fontWeight: '500',
+  fontFamily: 'Helvetica, sans-serif',
+})
 
-const Weight = styled("div")({
-   fontSize: "14px",
-  color: "gray",
-  fontFamily: "Helvetica, sans-serif",
-});
+const Weight = styled('div')({
+  fontSize: '14px',
+  color: 'gray',
+  fontFamily: 'Helvetica, sans-serif',
+})
 
-const Counter = styled("div")({
-  marginTop: "10px",
-  display: "flex",
-  justifyContent: "space-between",
-  alignItems: "center",
-  border: "1px solid #ccc",
-  borderRadius: "10px",
-  padding: "6px 10px",
-});
+const Counter = styled('div')({
+  marginTop: '10px',
+  height: '38px',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  border: '1px solid black',
+  borderRadius: '10px',
+  padding: '0 10px',
+})
