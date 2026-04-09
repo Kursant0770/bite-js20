d289379e61a07965438ec670ab723f3b8206eec8
mahamatalievelmar-arch <mahamatalievelmar@gmail.com>
2026-04-09 15:30:01 +0600

    fixed

--- src/components/ui/Select/Select.jsx
+++ src/components/ui/Select/Select.jsx
@@ -4,25 +4,22 @@ import InputLabel from '@mui/material/InputLabel'
 import { Select as MuiSelect } from '@mui/material'
 import { forwardRef } from 'react'
 
-export const Select = forwardRef(({ value, onChange, options, label }, ref) => {
-  return (
-    <FormControl size="small" style={{ minWidth: 200 }}>
-      <InputLabel id="sort-label">{label}</InputLabel>
-      <MuiSelect
-        ref={ref}
-        labelId="sort-label"
-        id="sort-select"
-        value={value}
-        label={label}
-        onChange={(e) => onChange(e.target.value)}
-      >
-        {options.map(({ value, label }) => (
-          <MenuItem key={value} value={value}>
-            {label}
-          </MenuItem>
-        ))}
-      </MuiSelect>
-    </FormControl>
-  )
-})
-
+export const Select = forwardRef(({ value, onChange, options, label }, ref) => (
+  <FormControl size="small" style={{ minWidth: 200 }}>
+    <InputLabel id="sort-label">{label}</InputLabel>
+    <MuiSelect
+      ref={ref}
+      labelId="sort-label"
+      id="sort-select"
+      value={value}
+      label={label}
+      onChange={(e) => onChange(e.target.value)}
+    >
+      {options.map(({ value, label }) => (
+        <MenuItem key={value} value={value}>
+          {label}
+        </MenuItem>
+      ))}
+    </MuiSelect>
+  </FormControl>
+))
